18c7d88702a94bef178cb12c3acb5b3a748f7f1a
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-19 20:46:39 -0400

    Minimal working example

--- lua/jest/init.lua
+++ lua/jest/init.lua
@@ -7,18 +7,36 @@ function M:setup(options)
 
 	options = options or default_options
 
-	vim.api.nvim_create_user_command('Jest', function ()
+	local function run_jest(callback)
 		vim.fn.jobstart('npx jest --json', {
-			on_stdout = function (_, data)
+			on_stdout = function(_, data)
+				local outputs = {}
 				for _, line in ipairs(data) do
 					local jest_output = models.JestOutput:new(line)
-					print(jest_output)
+					table.insert(outputs, jest_output)
 				end
+
+				callback(outputs)
 			end,
 			stdout_buffered = true
 		})
+	end
+
+	local output_bufnr = nil
+	local function display_output(output)
+		if output_bufnr == nil then
+			output_bufnr = vim.api.nvim_create_buf(false, true)
+			vim.cmd('vert sb' .. output_bufnr)
+		end
+
+		vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, vim.split(tostring(output), '\n'))
+	end
+
+	vim.api.nvim_create_user_command('Jest', function()
+		run_jest(function (outputs)
+			display_output(outputs[1])
+		end)
 	end, {})
 end
 
 return M
-
